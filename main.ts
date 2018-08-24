bb922989a5aa61f20025166f3d7c2a9f8ac56af4
unknown <1712876030@qq.com>
2018-08-24 16:47:43 +0800

    kk

--- main.ts
+++ main.ts
@@ -805,7 +805,7 @@ namespace mbit_小车类 {
     //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
     export function Min_Motor_Shake(): void {
          
-        setPwm(0, 0, 4000);
+        setPwm(0, 0, 0);
         setPwm(1, 0, 4000);
 　　　  setPwm(2, 0, 0);
         setPwm(3, 0, 0);
