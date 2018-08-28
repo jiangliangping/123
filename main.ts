43e78b6c221258d0e7c022fca21d47083fc0cead
unknown <1712876030@qq.com>
2018-08-28 10:55:17 +0800

    ff

--- main.ts
+++ main.ts
@@ -11,7 +11,28 @@ load dependency
 
 //% color="#808080" weight=23 icon="\uf11c"
 namespace mbit_输入类 {
+		
+		const PCA9685_ADD = 0x41
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
 
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const PRESCALE = 0xFE
+    
+    let initialized = false
+    
     export enum enRocker {
         //% blockId="Nostate" block="无"
         Nostate = 0,
@@ -27,6 +48,12 @@ namespace mbit_输入类 {
         Press
     }
 
+		 export enum Motorshock {
+				//% blockId="OFF" block="关"
+        OFF = 0,
+        //% blockId="ON" block="开"
+        ON
+			}
     export enum enButton {
         //% blockId="Press" block="按下"
         Press = 0,
@@ -43,6 +70,81 @@ namespace mbit_输入类 {
         //% blockId="yellow" block="黄色"
         yellow
     }
+    
+        function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        if (!initialized) {
+            initPCA9685();
+        }
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADD, buf);
+    }
+    
+    //% blockId=mbit_Min_Motor_Shake block="Min_Motor_Shake|value %value"
+    //% weight=101
+    //% blockGap=10
+    //% color="#C814B8"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Min_Motor_Shake(value: Motorshock): void {
+        switch (value) {
+        		case Motorshock.OFF: {
+        			setPwm(0, 0, 4000);
+        			break;
+        		}
+        		case Motorshock.ON: {
+        			setPwm(0, 0, 0);
+        			break;
+        		}
+        } 
+    }
+    
 		//% blockId=mbit_Push_botton block="Push_botton|value1 %value1|value %value"
     //% weight=100
     //% blockGap=10
