333823fce689c1b801dce609ac1f97936f4803b1
unknown <1712876030@qq.com>
2018-09-10 15:06:44 +0800

    gg

--- main.ts
+++ main.ts
@@ -104,7 +104,7 @@ namespace GHBit {
     
     export enum speed {
     	  //% blockId="speed0" block="0"
-        speed0 = 0,
+        speed0 = 100000,
         //% blockId="speed1" block="1"
         speed1 = 2000,
         //% blockId="speed2" block="2"
